97756d31ce05628ce78f987264ef46c3225c3666
Welt Xing <1753057600@qq.com>
2021-10-10 10:10:59 +0800

    支持多分类

---
+++ base_svc.py
@@ -0,0 +1,237 @@
+from solver import Solver, NuSolver
+import numpy as np
+from sklearn.base import BaseEstimator
+
+
+class LinearSVC(BaseEstimator):
+    def __init__(self, C=1, max_iter=1000, tol=1e-3, verbose=False) -> None:
+        self.max_iter = max_iter
+        self.C = C
+        self.tol = tol
+        self.verbose = verbose
+
+    def fit(self, X, y):
+        X, y = np.array(X), np.array(y, dtype=float)
+        if len(np.unique(y)) > 2:
+            print("LinearSVC only support binary classification")
+            return self
+
+        y[y != 1] = -1
+        l, self.n_features = X.shape
+
+        y = y.reshape(l, 1)
+        Q = (y @ y.T) * (X @ X.T)  # Q矩阵
+        y = y.reshape(-1)
+
+        s = Solver(l=l,
+                   Q=Q,
+                   p=-np.ones(l),
+                   y=y,
+                   alpha=np.zeros(l),
+                   Cp=self.C,
+                   Cn=self.C,
+                   max_iter=self.max_iter,
+                   eps=self.tol)
+        s.solve(self.verbose)
+        w = s.get_alpha() * y @ X
+        b = s.get_b()
+        self.decision_function = lambda x: w @ x.T + b
+        return self
+
+    def predict(self, X):
+        X = np.array(X).reshape(-1, self.n_features)  # (l * n_f)
+        pred = self.decision_function(X)
+        pred[pred > 0] = 1
+        pred[pred < 0] = 0
+        return pred.astype('int')
+
+    def score(self, X, y):
+        X = np.array(X).reshape(-1, self.n_features)
+        y = np.array(y).reshape(-1)
+        pred = self.predict(X)
+        return np.mean(pred == y)
+
+
+class KernelSVC(BaseEstimator):
+    def __init__(self,
+                 C=1,
+                 max_iter=1000,
+                 kernel='rbf',
+                 degree=3,
+                 gamma='scale',
+                 coef0=0,
+                 tol=1e-3,
+                 verbose=False) -> None:
+        super().__init__()
+        self.C = C
+        self.max_iter = 1000
+        self.kernel = kernel
+        self.gamma = gamma
+        self.degree = degree
+        self.coef0 = coef0
+        self.tol = tol
+        self.max_iter = max_iter
+        self.verbose = verbose
+
+    def fit(self, X, y):
+        X, y = np.array(X), np.array(y, dtype=float)
+        if len(np.unique(y)) > 2:
+            print("KernelSVC only support binary classification")
+            return self
+
+        y[y != 1] = -1
+        l, self.n_features = X.shape
+
+        # 注册核函数相关
+        if type(self.gamma) == float:
+            gamma = self.gamma
+        else:
+            gamma = {
+                'scale': 1 / (self.n_features * X.std()),
+                'auto': 1 / self.n_features,
+            }[self.gamma]
+        degree = self.degree
+        coef0 = self.coef0
+        self.kernel_func = {
+            "linear":
+            lambda x, y: x @ y.T,
+            "poly":
+            lambda x, y: (gamma * x @ y.T + coef0)**degree,
+            "rbf":
+            lambda x, y: np.exp(-gamma * np.linalg.norm(
+                np.expand_dims(x, axis=1) - y, axis=-1)**2),
+            "sigmoid":
+            lambda x, y: np.tanh(gamma * (x @ y.T) + coef0)
+        }[self.kernel]
+
+        # 计算Q
+        y = y.reshape(l, 1)
+        Q = (y @ y.T) * self.kernel_func(X, X)
+        y = y.reshape(-1)
+
+        s = Solver(
+            l=l,
+            Q=Q,
+            p=-np.ones(l),
+            y=y,
+            alpha=np.zeros(l),
+            Cp=self.C,
+            Cn=self.C,
+            max_iter=self.max_iter,
+            eps=self.tol,
+        )
+        s.solve(verbose=self.verbose)
+        alpha = s.get_alpha()
+        b = s.get_b()
+        self.decision_function = lambda x: alpha * y @ self.kernel_func(
+            X,
+            x,
+        ) + b
+        return self
+
+    def predict(self, X):
+        X = np.array(X).reshape(-1, self.n_features)
+        pred = self.decision_function(X)
+        pred[pred > 0] = 1
+        pred[pred < 0] = 0
+        return pred.astype('int')
+
+    def score(self, X, y):
+        X = np.array(X).reshape(-1, self.n_features)
+        y = np.array(y).reshape(-1)
+        pred = self.predict(X)
+        return np.mean(pred == y)
+
+
+class NuSVC(KernelSVC):
+    def __init__(
+        self,
+        nu=0.5,
+        max_iter=1000,
+        kernel='rbf',
+        degree=3,
+        gamma='scale',
+        coef0=0,
+        tol=1e-3,
+        verbose=False,
+    ) -> None:
+        self.max_iter = max_iter
+        self.nu = nu
+        self.kernel = kernel
+        self.gamma = gamma
+        self.degree = degree
+        self.coef0 = coef0
+        self.tol = tol
+        self.verbose = verbose
+
+    def fit(self, X, y):
+        X, y = np.array(X), np.array(y, dtype=float)
+        if len(np.unique(y)) > 2:
+            print("NuSVC only support binary classification")
+            return self
+
+        y[y != 1] = -1
+        l, self.n_features = X.shape
+
+        if type(self.gamma) == float:
+            gamma = self.gamma
+        else:
+            gamma = {
+                'scale': 1 / (self.n_features * X.std()),
+                'auto': 1 / self.n_features,
+            }[self.gamma]
+        degree = self.degree
+        coef0 = self.coef0
+        self.kernel_func = {
+            "linear":
+            lambda x, y: x @ y.T,
+            "poly":
+            lambda x, y: (gamma * x @ y.T + coef0)**degree,
+            "rbf":
+            lambda x, y: np.exp(-gamma * np.linalg.norm(
+                np.expand_dims(x, axis=1) - y, axis=-1)**2),
+            "sigmoid":
+            lambda x, y: np.tanh(gamma * (x @ y.T) + coef0)
+        }[self.kernel]
+
+        y = y.reshape(l, 1)
+        Q = (y @ y.T) * self.kernel_func(X, X)  # Q矩阵
+        y = y.reshape(-1)
+
+        # 计算alpha初始值
+        sum_pos = self.nu * l / 2
+        sum_neg = self.nu * l / 2
+        alpha = np.zeros(l)
+
+        for i in range(l):
+            if y[i] == +1:
+                alpha[i] = min(1., sum_pos)
+                sum_pos -= alpha[i]
+            else:
+                alpha[i] = min(1., sum_neg)
+                sum_neg -= alpha[i]
+
+        s = NuSolver(
+            l=l,
+            Q=Q,
+            p=np.zeros(l),
+            y=y,
+            alpha=alpha,
+            Cp=1,
+            Cn=1,
+            eps=self.tol,
+            max_iter=self.max_iter,
+        )
+        s.solve(verbose=self.verbose)
+        rho = s.get_rho()
+        alpha = s.get_alpha() / rho
+        b = s.get_b() / rho
+        self.decision_function = lambda x: alpha * y @ self.kernel_func(X, x
+                                                                        ) + b
+        return self
+
+    def predict(self, X):
+        return super().predict(X)
+
+    def score(self, X, y):
+        return super().score(X, y)
--- svc.py
+++ svc.py
@@ -1,58 +1,48 @@
-from solver import Solver, NuSolver
-import numpy as np
+import base_svc
+from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
 from sklearn.base import BaseEstimator
 
+multi_method = {
+    "ovo": OneVsOneClassifier,
+    "ovr": OneVsRestClassifier,
+}
 
-class LinearSVC(BaseEstimator):
-    def __init__(self, C=1, max_iter=1000, tol=1e-3, verbose=False) -> None:
+
+class MultiLinearSVC(BaseEstimator):
+    def __init__(self,
+                 C=1,
+                 max_iter=1000,
+                 tol=1e-3,
+                 verbose=False,
+                 n_jobs=None,
+                 method="ovo") -> None:
+        super().__init__()
         self.max_iter = max_iter
         self.C = C
         self.tol = tol
         self.verbose = verbose
+        self.n_jobs = n_jobs
+        self.method = method
+
+        self.base_model = base_svc.LinearSVC(C, max_iter, tol, verbose)
 
     def fit(self, X, y):
-        X, y = np.array(X), np.array(y, dtype=float)
-        if len(np.unique(y)) > 2:
-            print("LinearSVC only support binary classification")
-            return self
-
-        y[y != 1] = -1
-        l, self.n_features = X.shape
-
-        y = y.reshape(l, 1)
-        Q = (y @ y.T) * (X @ X.T)  # Q矩阵
-        y = y.reshape(-1)
-
-        s = Solver(l=l,
-                   Q=Q,
-                   p=-np.ones(l),
-                   y=y,
-                   alpha=np.zeros(l),
-                   Cp=self.C,
-                   Cn=self.C,
-                   max_iter=self.max_iter,
-                   eps=self.tol)
-        s.solve(self.verbose)
-        w = s.get_alpha() * y @ X
-        b = s.get_b()
-        self.decision_function = lambda x: w @ x.T + b
+        self.multi_model = multi_method[self.method](
+            estimator=self.base_model,
+            n_jobs=self.n_jobs,
+        )
+        self.multi_model.fit(X, y)
         return self
 
     def predict(self, X):
-        X = np.array(X).reshape(-1, self.n_features)  # (l * n_f)
-        pred = self.decision_function(X)
-        pred[pred > 0] = 1
-        pred[pred < 0] = 0
-        return pred.astype('int')
+        return self.multi_model.predict(X)
 
     def score(self, X, y):
-        X = np.array(X).reshape(-1, self.n_features)
-        y = np.array(y).reshape(-1)
         pred = self.predict(X)
-        return np.mean(pred == y)
+        return (pred == y).mean(0)
 
 
-class KernelSVC(BaseEstimator):
+class MultiKernelSVC(BaseEstimator):
     def __init__(self,
                  C=1,
                  max_iter=1000,
@@ -61,7 +51,9 @@ class KernelSVC(BaseEstimator):
                  gamma='scale',
                  coef0=0,
                  tol=1e-3,
-                 verbose=False) -> None:
+                 verbose=False,
+                 n_jobs=None,
+                 method="ovo") -> None:
         super().__init__()
         self.C = C
         self.max_iter = 1000
@@ -72,78 +64,37 @@ class KernelSVC(BaseEstimator):
         self.tol = tol
         self.max_iter = max_iter
         self.verbose = verbose
+        self.n_jobs = n_jobs
+        self.method = method
+
+        self.base_model = base_svc.KernelSVC(
+            C,
+            max_iter,
+            kernel,
+            degree,
+            gamma,
+            coef0,
+            tol,
+            verbose,
+        )
 
     def fit(self, X, y):
-        X, y = np.array(X), np.array(y, dtype=float)
-        if len(np.unique(y)) > 2:
-            print("KernelSVC only support binary classification")
-            return self
-
-        y[y != 1] = -1
-        l, self.n_features = X.shape
-
-        # 注册核函数相关
-        if type(self.gamma) == float:
-            gamma = self.gamma
-        else:
-            gamma = {
-                'scale': 1 / (self.n_features * X.std()),
-                'auto': 1 / self.n_features,
-            }[self.gamma]
-        degree = self.degree
-        coef0 = self.coef0
-        self.kernel_func = {
-            "linear":
-            lambda x, y: x @ y.T,
-            "poly":
-            lambda x, y: (gamma * x @ y.T + coef0)**degree,
-            "rbf":
-            lambda x, y: np.exp(-gamma * np.linalg.norm(
-                np.expand_dims(x, axis=1) - y, axis=-1)**2),
-            "sigmoid":
-            lambda x, y: np.tanh(gamma * (x @ y.T) + coef0)
-        }[self.kernel]
-
-        # 计算Q
-        y = y.reshape(l, 1)
-        Q = (y @ y.T) * self.kernel_func(X, X)
-        y = y.reshape(-1)
-
-        s = Solver(
-            l=l,
-            Q=Q,
-            p=-np.ones(l),
-            y=y,
-            alpha=np.zeros(l),
-            Cp=self.C,
-            Cn=self.C,
-            max_iter=self.max_iter,
-            eps=self.tol,
+        self.multi_model = multi_method[self.method](
+            estimator=self.base_model,
+            n_jobs=self.n_jobs,
         )
-        s.solve(verbose=self.verbose)
-        alpha = s.get_alpha()
-        b = s.get_b()
-        self.decision_function = lambda x: alpha * y @ self.kernel_func(
-            X,
-            x,
-        ) + b
+        self.multi_model.fit(X, y)
         return self
 
     def predict(self, X):
-        X = np.array(X).reshape(-1, self.n_features)
-        pred = self.decision_function(X)
-        pred[pred > 0] = 1
-        pred[pred < 0] = 0
-        return pred.astype('int')
+        return self.multi_model.predict(X)
 
     def score(self, X, y):
-        X = np.array(X).reshape(-1, self.n_features)
-        y = np.array(y).reshape(-1)
         pred = self.predict(X)
-        return np.mean(pred == y)
+        return (pred == y).mean(0)
 
 
-class NuSVC(KernelSVC):
+class MultiNuSVC(BaseEstimator):
     def __init__(
         self,
         nu=0.5,
@@ -154,7 +105,10 @@ class NuSVC(KernelSVC):
         coef0=0,
         tol=1e-3,
         verbose=False,
+        n_jobs=None,
+        method="ovo",
     ) -> None:
+        super().__init__()
         self.max_iter = max_iter
         self.nu = nu
         self.kernel = kernel
@@ -163,75 +117,35 @@ class NuSVC(KernelSVC):
         self.coef0 = coef0
         self.tol = tol
         self.verbose = verbose
+        self.n_jobs = n_jobs
+        self.method = method
+        self.base_model = base_svc.NuSVC(
+            nu,
+            max_iter,
+            kernel,
+            degree,
+            gamma,
+            coef0,
+            tol,
+            verbose,
+        )
 
     def fit(self, X, y):
-        X, y = np.array(X), np.array(y, dtype=float)
-        if len(np.unique(y)) > 2:
-            print("NuSVC only support binary classification")
-            return self
-
-        y[y != 1] = -1
-        l, self.n_features = X.shape
-
-        if type(self.gamma) == float:
-            gamma = self.gamma
-        else:
-            gamma = {
-                'scale': 1 / (self.n_features * X.std()),
-                'auto': 1 / self.n_features,
-            }[self.gamma]
-        degree = self.degree
-        coef0 = self.coef0
-        self.kernel_func = {
-            "linear":
-            lambda x, y: x @ y.T,
-            "poly":
-            lambda x, y: (gamma * x @ y.T + coef0)**degree,
-            "rbf":
-            lambda x, y: np.exp(-gamma * np.linalg.norm(
-                np.expand_dims(x, axis=1) - y, axis=-1)**2),
-            "sigmoid":
-            lambda x, y: np.tanh(gamma * (x @ y.T) + coef0)
-        }[self.kernel]
-
-        y = y.reshape(l, 1)
-        Q = (y @ y.T) * self.kernel_func(X, X)  # Q矩阵
-        y = y.reshape(-1)
-
-        # 计算alpha初始值
-        sum_pos = self.nu * l / 2
-        sum_neg = self.nu * l / 2
-        alpha = np.zeros(l)
-
-        for i in range(l):
-            if y[i] == +1:
-                alpha[i] = min(1., sum_pos)
-                sum_pos -= alpha[i]
-            else:
-                alpha[i] = min(1., sum_neg)
-                sum_neg -= alpha[i]
-
-        s = NuSolver(
-            l=l,
-            Q=Q,
-            p=np.zeros(l),
-            y=y,
-            alpha=alpha,
-            Cp=1,
-            Cn=1,
-            eps=self.tol,
-            max_iter=self.max_iter,
+        self.multi_model = multi_method[self.method](
+            estimator=self.base_model,
+            n_jobs=self.n_jobs,
         )
-        s.solve(verbose=self.verbose)
-        rho = s.get_rho()
-        alpha = s.get_alpha() / rho
-        b = s.get_b() / rho
-        self.decision_function = lambda x: alpha * y @ self.kernel_func(X, x
-                                                                        ) + b
+        self.multi_model.fit(X, y)
         return self
 
     def predict(self, X):
-        return super().predict(X)
+        return self.multi_model.predict(X)
 
     def score(self, X, y):
-        return super().score(X, y)
+        pred = self.predict(X)
+        return (pred == y).mean(0)
+
+
+LinearSVC = MultiLinearSVC
+KernelSVC = MultiKernelSVC
+NuSVC = MultiKernelSVC
