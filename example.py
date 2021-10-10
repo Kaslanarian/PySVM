1ae0aae4fee2f78610ce799b7a302b59ab110402
Welt Xing <1753057600@qq.com>
2021-10-10 10:11:07 +0800

    加入example

--- example.py
+++ example.py
@@ -1,98 +1,174 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.font_manager
-from sklearn import svm
+from matplotlib.font_manager import FontProperties
+from sklearn.datasets import *
+from sklearn.model_selection import train_test_split
+from svc import LinearSVC, KernelSVC, NuSVC
+from svr import LinearSVR, KernelSVR, NuSVR
 from one_class import OneClassSVM
 
-# 背景坐标点阵
-xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
 
-# 生成训练数据
-X = 0.3 * np.random.randn(100, 2)  # 100个正常数据，shape=(100,2),[0,1)之间
-X_train = np.r_[X + 2, X - 2]  # 向左侧平移2得到一组数据，向右侧平移2得到一组数据，两组数据串联，
+def visual_svc_test():
+    X, y = make_classification(
+        n_features=2,
+        n_classes=3,
+        n_informative=2,
+        n_redundant=0,
+        n_repeated=0,
+        n_clusters_per_class=1,
+    )
+    for y_ in np.unique(y):
+        data = X[y == y_]
+        plt.scatter(data[:, 0], data[:, 1], label="%d" % y_)
 
-# 生成测试数据
-X = 0.3 * np.random.randn(20, 2)  # 20个异常数据
-X_test = np.r_[X + 2, X - 2]  # 向左侧平移2得到一组数据，向右侧平移2得到一组数据，两组数据串联，
+    plt.legend()
+    plt.show()
 
-# 生成20个异常数据，
-X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+    train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.7)
+    print(("{}SVC's perf : {:.2f}%\n" * 3).format(
+        "Linear",
+        LinearSVC().fit(train_X, train_y).score(test_X, test_y) * 100,
+        "Kernel",
+        KernelSVC().fit(train_X, train_y).score(test_X, test_y) * 100,
+        "    Nu",
+        NuSVC().fit(train_X, train_y).score(test_X, test_y) * 100,
+    ))
 
-# 训练模型
-clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
-clf.fit(X_train)
 
-#----》判断数据是在超平面内还是超平面外，返回+1或-1，正号是超平面内，负号是在超平面外
-y_pred_train = clf.predict(X_train)
-y_pred_test = clf.predict(X_test)
-y_pred_outliers = clf.predict(X_outliers)
+def dataset_svc_test(dataset="digits"):
+    X, y = {
+        "iris": load_iris,
+        "breast_cancer": load_breast_cancer,
+        "wine": load_wine,
+        "digits": load_digits,
+    }[dataset](return_X_y=True)
+    X = (X - X.mean(0)) / (X.std(0) + 1e-8)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.7)
+    print(("dataset : load_{}\n" + "{}SVC's perf : {:.2f}%\n" * 3).format(
+        dataset,
+        "Linear",
+        LinearSVC(n_jobs=4).fit(train_X, train_y).score(test_X, test_y) * 100,
+        "Kernel",
+        KernelSVC(n_jobs=4).fit(train_X, train_y).score(test_X, test_y) * 100,
+        "    Nu",
+        NuSVC(n_jobs=4).fit(train_X, train_y).score(test_X, test_y) * 100,
+    ))
 
-#----》计算样本到超平面的距离，含正负号，正好表示在超平面内，负号表示在超平面外
-y_deci_train = clf.decision_function(X_train)
-y_deci_test = clf.decision_function(X_test)
-y_deci_outliers = clf.decision_function(X_outliers)
 
-# 统计预测错误的个数
-n_error_train = y_pred_train[y_pred_train == -1].size
-n_error_test = y_pred_test[y_pred_test == -1].size
-n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+def visual_svr_test():
+    X, y = make_regression(n_features=1, noise=3, n_samples=50, random_state=1)
+    plt.scatter(X.reshape(-1), y, label="linear_data")
+    plt.scatter(X.reshape(-1), 0.01 * y**2, label="squared_data")
 
-# 计算网格数据到超平面的距离，含正负号
-Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])  # ravel表示数组拉直
-Z = Z.reshape(xx.shape)
-"""
-绘图
-"""
-plt.title("Novelty Detection")
-plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), 0, 7),
-             cmap=plt.cm.PuBu)  #绘制异常区域的轮廓， 把异常区域划分为7个层次
-a = plt.contour(xx, yy, Z, levels=[0], linewidths=2,
-                colors='darkred')  # 绘制轮廓，SVM的边界点（到边界距离为0的点
-plt.contourf(xx, yy, Z, levels=[0, Z.max()],
-             colors='palevioletred')  # 绘制正常样本的区域，使用带有填充的轮廓
+    model = LinearSVR(C=10)
+    model.fit(X, y)
+    test_x = np.linspace(X.min(0), X.max(0), 2)
+    pred = model.predict(test_x)
+    plt.plot(test_x, pred, label="LinearSVC", color='red')
 
-s = 40  # 样本点的尺寸大小
-b1 = plt.scatter(X_train[:, 0], X_train[:, 1], c='white', s=s,
-                 edgecolors='k')  # 绘制训练样本，填充白色，边缘”k“色
-b2 = plt.scatter(X_test[:, 0],
-                 X_test[:, 1],
-                 c='blueviolet',
-                 s=s,
-                 edgecolors='k')  # 绘制测试样本--正常样本，填充蓝色，边缘”k“色
-c = plt.scatter(X_outliers[:, 0],
-                X_outliers[:, 1],
-                c='gold',
-                s=s,
-                edgecolors='k')  # 绘制测试样本--异常样本，填充金色，边缘”k“色
+    model = KernelSVR(C=100, kernel='poly', degree=2)
+    model.fit(X, 0.01 * y**2)
+    test_x = np.linspace(X.min(0), X.max(0), 100)
+    pred = model.predict(test_x)
+    plt.plot(test_x, pred, label="KernelSVC", color='yellowgreen')
 
-plt.axis('tight')
-plt.xlim((-5, 5))
-plt.ylim((-5, 5))
+    plt.legend()
+    plt.show()
 
-# 集中添加图注
-plt.legend([a.collections[0], b1, b2, c], [
-    "learned frontier", "training data", "test regular data",
-    "test abnormal data"
-],
-           loc="upper left",
-           prop=matplotlib.font_manager.FontProperties(size=11))
-plt.xlabel(
-    "error train: %d/200 ;   errors novel regular: %d/40 ;   errors novel abnormal: %d/40"
-    % (n_error_train, n_error_test, n_error_outliers))
-plt.show()
-# %%
-import matplotlib.pyplot as plt
-import numpy as np
-from one_class import OneClassSVM
-import seaborn as sns
 
-sns.set()
-X_train = np.random.randn(1000)
-X_train[:10] = 0.001 * np.random.randn(10) + 10
-sns.histplot(X_train)
-# %%
-X_test = [-10, 0, 10]
-model = OneClassSVM()
-model.fit(X_train.reshape(-1, 1))
-# %%
-model.predict(X_test)
+def dataset_svr_test(dataset="boston"):
+    X, y = {
+        "boston": load_boston,
+        "diabetes": load_diabetes,
+    }[dataset](return_X_y=True)
+    X = (X - X.mean(0)) / (X.std(0) + 1e-8)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, train_size=0.7)
+    print(("dataset : load_{}\n" + "{}SVR's perf : {}\n" * 3).format(
+        dataset,
+        "Linear",
+        -LinearSVR().fit(train_X, train_y).score(test_X, test_y),
+        "Kernel",
+        -KernelSVR().fit(train_X, train_y).score(test_X, test_y),
+        "    Nu",
+        -NuSVR().fit(train_X, train_y).score(test_X, test_y),
+    ))
+
+
+def visual_one_class_test():
+    xx, yy = np.meshgrid(np.linspace(-5, 5, 500), np.linspace(-5, 5, 500))
+
+    X = 0.3 * np.random.randn(100, 2)
+    X_train = np.r_[X + 2, X - 2]
+
+    X = 0.3 * np.random.randn(20, 2)
+    X_test = np.r_[X + 2, X - 2]
+
+    X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+
+    clf = OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+    clf.fit(X_train)
+
+    y_pred_train = clf.predict(X_train)
+    y_pred_test = clf.predict(X_test)
+    y_pred_outliers = clf.predict(X_outliers)
+
+    n_error_train = y_pred_train[y_pred_train == -1].size
+    n_error_test = y_pred_test[y_pred_test == -1].size
+    n_error_outliers = y_pred_outliers[y_pred_outliers == 1].size
+
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    plt.title("Novelty Detection")
+    plt.contourf(xx,
+                 yy,
+                 Z,
+                 levels=np.linspace(Z.min(), 0, 7),
+                 cmap=plt.cm.PuBu)
+    a = plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='darkred')
+    plt.contourf(xx, yy, Z, levels=[0, Z.max()], colors='palevioletred')
+
+    s = 40
+    b1 = plt.scatter(X_train[:, 0],
+                     X_train[:, 1],
+                     c='white',
+                     s=s,
+                     edgecolors='k')
+    b2 = plt.scatter(X_test[:, 0],
+                     X_test[:, 1],
+                     c='blueviolet',
+                     s=s,
+                     edgecolors='k')
+    c = plt.scatter(X_outliers[:, 0],
+                    X_outliers[:, 1],
+                    c='gold',
+                    s=s,
+                    edgecolors='k')
+
+    plt.axis('tight')
+    plt.xlim((-5, 5))
+    plt.ylim((-5, 5))
+
+    plt.legend([a.collections[0], b1, b2, c], [
+        "learned frontier", "training data", "test regular data",
+        "test abnormal data"
+    ],
+               loc="upper left",
+               prop=FontProperties(size=11))
+    plt.xlabel(
+        "error train: %d/200 ;   errors novel regular: %d/40 ;   errors novel abnormal: %d/40"
+        % (n_error_train, n_error_test, n_error_outliers))
+    plt.show()
+
+
+if __name__ == "__main__":
+    print("自制三分类数据进行分类：")
+    visual_svc_test()
+    print("对sklearn.datasets中的数据集进行分类：")
+    dataset_svc_test()
+    print("自制数据集进行回归：")
+    visual_svr_test()
+    print("对sklearn.datasets中的数据集进行分类：")
+    dataset_svr_test()
+    print("测试并可视化OneClassSVM的训练效果：")
+    visual_one_class_test()
